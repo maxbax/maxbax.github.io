96ddb3de5e0be941ff75fe288b1ed27200d67758
Max <maxbax666@gmail.com>
2026-02-07 17:51:42 +0100

    New simple landing page

--- scripts/app.js
+++
@@ -1,9 +0,0 @@
-angular.module("ECApp", ['ngAnimate', 'angular-inview']);
-
-angular.module("ECApp").directive('initAfterDelay', function($timeout) {
-	return function initAfterDelayLink(scope, element, attrs) {
-		$timeout(function() {
-			scope.$eval(attrs.initAfterDelay);
-		}, parseInt(attrs.delay) || 1000);
-	}
-});
